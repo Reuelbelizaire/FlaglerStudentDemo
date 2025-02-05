d2912c34db7b84ba933dbccd5057aaf20dffffaf
Reuel Belizaire <Belizaire347@flagler.edu>
2025-02-05 14:44:40 -0500

    Initial Commit

---
+++ FlaglerStudentDemoTests/FlaglerStudentDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlaglerStudentDemoTests.swift
+//  FlaglerStudentDemoTests
+//
+//  Created by Belizaire, Reuel James on 1/29/25.
+//
+
+import Testing
+@testable import FlaglerStudentDemo
+
+struct FlaglerStudentDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
